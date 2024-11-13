ec3a7a7f442ae040a1918919d946f817de953011
Anhphap2004 <phapmeo2k4@gmail.com>
2024-11-13 23:30:48 +0700

    up code sau khi hoàn thành bài thực hành 11

--- Harmic/Controllers/ProductController.cs
+++ Harmic/Controllers/ProductController.cs
@@ -31,6 +31,7 @@ namespace Harmic.Controllers
             }
             ViewBag.ProductReview=_context.TbProductReviews.Where(i=>i.ProductId==id && i.IsActive).ToList();
             ViewBag.ProductRelated = _context.TbProducts.Where(i=>i.ProductId != id && i.CategoryProductId == product.CategoryProductId).Take(5).OrderByDescending(i=>i.ProductId).ToList();
+            ViewBag.CategoryProduct = _context.TbProductCategories.Where(i=>i.CategoryProductId == product.CategoryProductId && i.IsActive).ToList();
             return View(product);
         }
     }
